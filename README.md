16d6698c72d638a5b640f7da27735ab7cbc49e2f
Chris Wilkes <cwilkes@gmail.com>
2014-04-09 16:32:07 -0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+General thoughts about elevator problem
---
+++ src/main/java/com/ladro/ecs2/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.ladro.ecs2;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
